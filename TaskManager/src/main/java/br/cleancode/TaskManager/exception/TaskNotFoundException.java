2f9e3d709e644460d3b0b514fdb398d294d1bb59
Gabriel <gabrielconceicao569@gmail.com>
2025-05-10 18:16:37 -0300

    Update

--- TaskManager/src/main/java/br/cleancode/TaskManager/exception/TaskNotFoundException.java
+++ TaskManager/src/main/java/br/cleancode/TaskManager/exception/TaskNotFoundException.java
@@ -1,5 +1,9 @@
 package br.cleancode.TaskManager.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class TaskNotFoundException extends RuntimeException {
     public TaskNotFoundException(Long id) {
         super("Task com ID " + id + " não encontrada");
